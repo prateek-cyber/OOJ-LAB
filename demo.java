19c28fec9ab6c2a29cb767021abc8ad20d909881
prateek-cyber <67386701+prateek-cyber@users.noreply.github.com>
2020-11-20 13:53:37 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,7 @@
+import pp1.*;
+class demo{
+	public static void main(String args[]){
+		main2 m1=new main2();
+		m1.display();
+	}
+}
---
+++ main2.java
@@ -0,0 +1,7 @@
+package pp1;
+public class main2{
+	public int a = 4;
+	public void display(){
+		System.out.println("result= "+a);
+	}
+}
